80e6ef7d24348d50fae7b68d520acac9b821bc39
ben_graeler (Benedikt Gräler) <b.graeler@52north.org>
2012-01-05 09:05:54 +0000

    BB1copula added
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spcopula@33 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/BB1copula.R
@@ -0,0 +1,161 @@
+#####################
+##                 ##
+## the BB1 copulas ##
+##                 ##
+#####################
+# Joe, H., (1997). Multivariate Models and Dependence Concepts. Monogra. Stat. Appl. Probab. 73, London: Chapman and Hall. 
+
+validBB1Copula = function(object) {
+  if (object@dimension != 2)
+    return("Only BB1 copulas of dimension 2 are supported.")
+  param <- object@parameters
+  upper <- object@param.upbnd
+  lower <- object@param.lowbnd
+  if (length(param) != length(upper))
+    return("Parameter and upper bound have non-equal length")
+  if (length(param) != length(lower))
+    return("Parameter and lower bound have non-equal length")
+  if (any(is.na(param) | param >= upper | param[1] < lower[1] | param[2] <= lower[2]))
+    return("Parameter value out of bound.")
+  else return (TRUE)
+}
+
+setClass("BB1Copula",
+  representation = representation("copula", family="numeric"),
+  validity = validBB1Copula,
+  contains = list("copula")
+)
+
+# constructor
+BB1Copula <- function (param) {
+    val <- new("BB1Copula", dimension = 2, parameters = param, 
+        param.names = c("theta", "delta"), param.lowbnd = c(0, 1), param.upbnd = c(Inf, Inf), family=7, message = "BB1 copula family. Number 8 in CDVine.")
+    val
+}
+
+## density ##
+setMethod("dcopula", signature("BB1Copula"), linkCDVine.PDF)
+
+## jcdf ##
+setMethod("pcopula", signature("BB1Copula"), linkCDVine.CDF)
+
+## partial derivatives ##
+# ddu
+setMethod("dducopula", signature("BB1Copula"), linkCDVine.ddu)
+
+# ddv
+setMethod("ddvcopula", signature("BB1Copula"), linkCDVine.ddv)
+
+## random number generater ??
+setMethod("rcopula", signature("BB1Copula"), linkCDVine.r)
+
+#########################
+## BB1 survival copula ##
+#########################
+
+setClass("surBB1Copula",
+  representation = representation("copula", family="numeric"),
+  validity = validBB1Copula,
+  contains = list("copula")
+)
+
+# constructor
+surBB1Copula <- function (param) {
+  val <- new("surBB1Copula", dimension = 2, parameters = param, param.names = c("theta", "delta"), param.lowbnd = c(0, 1), param.upbnd = c(Inf, Inf), family=17, message = "Survival BB1 copula family. Number 18 in CDVine.")
+  val
+}
+
+## density ##
+setMethod("dcopula", signature("surBB1Copula"), linkCDVine.PDF)
+
+## jcdf ##
+setMethod("pcopula", signature("surBB1Copula"), linkCDVine.surCDF)
+  
+## partial derivatives ##
+# ddu
+setMethod("dducopula", signature("surBB1Copula"), linkCDVine.ddu)
+
+# ddv
+setMethod("ddvcopula", signature("surBB1Copula"), linkCDVine.ddv)
+
+## random number generater ??
+setMethod("rcopula", signature("surBB1Copula"), linkCDVine.r)
+
+####################
+## BB1 copula 90� ##
+####################
+
+validRotBB1Copula = function(object) {
+  if (object@dimension != 2)
+    return("Only BB1 copulas of dimension 2 are supported.")
+  param <- object@parameters
+  upper <- object@param.upbnd
+  lower <- object@param.lowbnd
+  if (length(param) != length(upper))
+    return("Parameter and upper bound have non-equal length")
+  if (length(param) != length(lower))
+    return("Parameter and lower bound have non-equal length")
+  if (any(is.na(param) | param[1] >= upper[1] | param[2] > upper[2] | param <= lower))
+    return("Parameter value out of bound")
+  else return (TRUE)
+}
+
+setClass("r90BB1Copula",
+  representation = representation("copula", family="numeric"),
+  validity = validRotBB1Copula,
+  contains = list("copula")
+)
+
+# constructor
+r90BB1Copula <- function (param) {
+  val <- new("r90BB1Copula", dimension = 2, parameters = param, param.names = c("theta", "delta"), param.lowbnd = c(-Inf, -Inf), param.upbnd = c(0, -1), family=27, message = "90° rotated BB1 copula family. Number 28 in CDVine.")
+  val
+}
+
+## density ##
+setMethod("dcopula", signature("r90BB1Copula"), linkCDVine.PDF)
+
+## jcdf ##
+setMethod("pcopula", signature("r90BB1Copula"), linkCDVine.r90CDF)
+
+## partial derivatives ##
+# ddu
+setMethod("dducopula", signature("r90BB1Copula"), linkCDVine.ddu)
+
+## ddv
+setMethod("ddvcopula", signature("r90BB1Copula"), linkCDVine.ddv)
+
+## random number generator
+setMethod("rcopula", signature("r90BB1Copula"), linkCDVine.r)
+
+#####################
+## BB1 copula 270� ##
+#####################
+
+setClass("r270BB1Copula",
+  representation = representation("copula", family="numeric"),
+  validity = validRotBB1Copula,
+  contains = list("copula")
+)
+
+# constructor
+r270BB1Copula <- function (param) {
+  val <- new("r270BB1Copula", dimension = 2, parameters = param, param.names = c("theta", "delta"), param.lowbnd = c(-Inf, -Inf), param.upbnd = c(0, -1), family=37, message = "270° rotated BB1 copula family. Number 38 in CDVine.")
+  val
+}
+
+## density ##
+setMethod("dcopula", signature("r270BB1Copula"), linkCDVine.PDF)
+
+## jcdf ##
+setMethod("pcopula", signature("r270BB1Copula"), linkCDVine.r270CDF)
+  
+## partial derivatives ##
+# ddu
+setMethod("dducopula", signature("r270BB1Copula"), linkCDVine.ddu)
+
+# ddv
+setMethod("ddvcopula", signature("r270BB1Copula"), linkCDVine.ddv)
+
+## random number generator
+setMethod("rcopula", signature("r270BB1Copula"), linkCDVine.r)
