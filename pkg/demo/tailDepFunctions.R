97c783be88fd9489eee7188fee934c6c68afb7d9
ben_graeler (Benedikt Gräler) <b.graeler@52north.org>
2015-11-26 10:30:03 +0000

    - fixed tailDepFunction demo after repeated remaining of tailDepFun to bivJointDepFun
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/spcopula@154 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/demo/tailDepFunctions.R
+++ pkg/demo/tailDepFunctions.R
@@ -6,20 +6,20 @@ rtPair <- 1-as.matrix(rankTransform(triples[,c(1,3)]))
 
 plot(rtPair,asp=1)
 
-tdfEmp <- empTailDepFun(rtPair)
+tdfEmp <- empBivJointDepFun(rtPair)
 plot(tdfEmp,ylim=c(0,1), ylab="tail index", xlab="u")
 abline(v=0.5, col="grey")
 
 gaussCop <- fitCopula(normalCopula(0), rtPair)@copula
-tdfGauss <- tailDepFun(gaussCop)
+tdfGauss <- bivJointDepFun(gaussCop)
 curve(tdfGauss, add=T,col="green",n=500)
 
 gumbelCop <- fitCopula(gumbelCopula(2),rtPair)@copula
-tdfGumbel <- tailDepFun(gumbelCop)
+tdfGumbel <- bivJointDepFun(gumbelCop)
 curve(tdfGumbel,add=T, col="blue",n=500)
 
 BB6Cop <- fitCopula(BB6Copula(), rtPair)@copula
-tdfBB6 <- tailDepFun(BB6Cop)
+tdfBB6 <- bivJointDepFun(BB6Cop)
 curve(tdfBB6, add=T,col="red",n=500)
 
 legend("bottomright",
